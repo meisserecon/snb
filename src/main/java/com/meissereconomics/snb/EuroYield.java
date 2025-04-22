f96456c025581565422fa9971e7664f14683870b
Luzius Meisser <luzius@aktionariat.com>
2025-04-22 11:46:00 +0200

    Update with data including 2025-04-17

--- src/main/java/com/meissereconomics/snb/EuroYield.java
+++ src/main/java/com/meissereconomics/snb/EuroYield.java
@@ -14,7 +14,7 @@ public class EuroYield extends TimeSeries {
 	public EuroYield(ForexData eur) throws FileNotFoundException, IOException {
 		super("Euro Bonds");
 		// From https://data.ecb.europa.eu/data/datasets/YC/YC.B.U2.EUR.4F.G_N_A.SV_C_YM.SR_5Y
-		List<String> lines = Files.readAllLines(FileSystems.getDefault().getPath("data", "ECB Data Portal_20250405084013.csv"));
+		List<String> lines = Files.readAllLines(FileSystems.getDefault().getPath("data", "ECB Data Portal.csv"));
 		LocalDate prevTime = null;
 		double prevValue = 0.0;
 		for (int i = 1; i < lines.size(); i++) {
